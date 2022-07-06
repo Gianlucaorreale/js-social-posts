35d3d501a96194b9556edc98e77f1c355fa486b9
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-07-06 16:59:21 +0200

    milestone-2

--- js/script.js
+++ js/script.js
@@ -20,14 +20,56 @@ Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e inc
 const posts = [
     {
         name: 'Phil Mangione',
-        postDate: '25/06/2021',
-        photo: 'kristopher-roller-PC_lbSSxCZE-unsplash.jpg'
+        postDate: '06/25/2021',
+        image: 'kristopher-roller-PC_lbSSxCZE-unsplash.jpg',
+        text: ' Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint'
     },
 
     {
         name: 'Bruce Banner',
-        postDate: '20/03/2021',
-        photo: 'diego-ph-fIq0tET6llw-unsplash.jpg'
+        postDate: '03/20/2021',
+        image: 'diego-ph-fIq0tET6llw-unsplash.jpg',
+        text: ' Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint'
     }
     
 ]
+
+const postsList = document.querySelector('posts-list')
+
+let cards = '';
+for (let i = 0; i < posts.length; i++){
+    const postsContainer = posts[i]
+    cards += ` 
+    <div class="post">
+      <div class="post__header">
+        <div class="post-meta">
+          <div class="post-meta__icon">
+            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione" />
+          </div>
+          <div class="post-meta__data">
+            <div class="post-meta__author">${posts.name}</div>
+            <div class="post-meta__time">4 mesi fa</div>
+          </div>
+        </div>
+      </div>
+      <div class="post__text">
+      ${posts.text}
+      </div>
+      <div class="post__image">
+        <img src="img/${posts.image}" alt="${posts.image}" />
+      </div>
+      <div class="post__footer">
+        <div class="likes js-likes">
+          <div class="likes__cta">
+            <a class="like-button js-like-button" href="#" data-postid="1">
+              <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+              <span class="like-button__label">Mi Piace</span>
+            </a>
+          </div>
+          <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone</div>
+        </div>
+      </div>
+    </div> `
+
+}
+postsList.innerHTML = cards;
